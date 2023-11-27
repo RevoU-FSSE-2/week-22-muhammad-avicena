121ba65f487b8ce5e960432be88e4715954db286
Muhammad Avicena <cenarahmant.dev@gmail.com>
2023-11-28 00:52:13 +0700

    feat: add user api

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from infrastructure.db import db, db_init
 from home.apis import home_blueprint
 from auth.apis import auth_blueprint
+from user.apis import user_blueprint
 import os
 
 app = Flask(__name__)
@@ -11,7 +12,7 @@ db.init_app(app)
 
 app.register_blueprint(home_blueprint, url_prefix="/")
 app.register_blueprint(auth_blueprint, url_prefix="/api/v1/auth")
-# app.register_blueprint(user_blueprint, url_prefix="/user")
+app.register_blueprint(user_blueprint, url_prefix="/api/v1/users")
 # app.register_blueprint(tweet_blueprint, url_prefix="/tweet")
 
 # with app.app_context():
