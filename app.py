c717d886a28220c992a1e483017980702182d17a
Muhammad Avicena <cenarahmant.dev@gmail.com>
2023-11-28 00:57:20 +0700

    feat: add CORS and Talisman

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from flask_cors import CORS
+from flask_talisman import Talisman
+
 from infrastructure.db import db, db_init
 from home.apis import home_blueprint
 from auth.apis import auth_blueprint
@@ -6,6 +9,8 @@ from user.apis import user_blueprint
 import os
 
 app = Flask(__name__)
+CORS(app)
+Talisman(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 db.init_app(app)
@@ -13,7 +18,6 @@ db.init_app(app)
 app.register_blueprint(home_blueprint, url_prefix="/")
 app.register_blueprint(auth_blueprint, url_prefix="/api/v1/auth")
 app.register_blueprint(user_blueprint, url_prefix="/api/v1/users")
-# app.register_blueprint(tweet_blueprint, url_prefix="/tweet")
 
 # with app.app_context():
 #     db_init()
