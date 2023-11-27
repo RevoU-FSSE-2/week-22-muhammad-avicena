7ff07d6b82195d40a24880b9b511549e718864fa
Muhammad Avicena <cenarahmant.dev@gmail.com>
2023-11-28 00:51:51 +0700

    chore: modified get user profile api

--- user/apis.py
+++ user/apis.py
@@ -1,33 +1,28 @@
-from flask import Blueprint, request
+from flask import Blueprint
 from auth.utils import user_required
-import jwt, os
 from user.models import User
-from auth.utils import decode_jwt
 
 user_blueprint = Blueprint("user", __name__)
 
 @user_blueprint.route("", methods=["GET"])
 @user_required
-def get_user_profile():
-    authorization_header = request.headers.get('Authorization')
-    token = authorization_header.split("Bearer ")[1] if "Bearer " in authorization_header else None
-    payload = decode_jwt(token)
-    print("Decoded Payload:", payload)
-    if not payload:
-        return {"error": "Token tidak valid"}, 401
-
-    user_id = payload.get("user_id")
+def get_user_profile(user_id):
 
     if not user_id:
-        return {"error": "User ID tidak ditemukan"}, 401
+        return {"error": "User not found", 'success': False }, 404
 
     user = User.query.get(user_id)
 
     if not user:
-        return {"error": "User tidak ditemukan!"}, 404
+         return {"error": "User not found", 'success': False }, 404
     
     return {
-        'id': user.id,
-        'username': user.username,
-        'bio': user.bio,
+        'success': True,
+        'message': 'Successfully get user profile',
+        'data': {
+            'id': user.id,
+            'username': user.username,
+            'email': user.email,
+            'role': user.role.value
+        }
     }
