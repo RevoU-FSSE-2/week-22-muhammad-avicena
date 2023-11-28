3e2557314101eca6b32d78188b259196ed8ec0ed
Muhammad Avicena <cenarahmant.dev@gmail.com>
2023-11-28 20:50:37 +0700

    feat: add todolist blueprint

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from infrastructure.db import db, db_init
 from home.apis import home_blueprint
 from auth.apis import auth_blueprint
 from user.apis import user_blueprint
+from todolist.apis import todolist_blueprint
 import os
 
 app = Flask(__name__)
@@ -18,6 +19,7 @@ db.init_app(app)
 app.register_blueprint(home_blueprint, url_prefix="/")
 app.register_blueprint(auth_blueprint, url_prefix="/api/v1/auth")
 app.register_blueprint(user_blueprint, url_prefix="/api/v1/users")
+app.register_blueprint(todolist_blueprint, url_prefix="/api/v1/todos")
 
 # with app.app_context():
 #     db_init()
