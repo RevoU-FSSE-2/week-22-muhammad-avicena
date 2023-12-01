1445a01e42f5183455b9ec6e92af9708f65858e4
Muhammad Avicena <cenarahmant.dev@gmail.com>
2023-12-02 00:59:40 +0700

    chore: remove unnecessary files

--- home/apis.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-
-home_blueprint = Blueprint("home", __name__)    
-
-@home_blueprint.route('/')
-def home():
-    return {"message": "Developed by M.A.R", "success": True}
-
